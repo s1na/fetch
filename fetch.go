fd2f127187e50a0d489a6d66e562f8148bf00c55
Sina Mahmoodi <itz.s1na@gmail.com>
2013-12-09 21:24:54 +0330

    Improve addToken to keep a postings list for each token in dictionary.

--- fetch.go
+++ fetch.go
@@ -7,12 +7,15 @@ import (
 	"io"
 	"os"
 	//"errors"
+	"container/list"
 	"sync"
 
 	"github.com/reiver/go-porterstemmer"
 )
 
 var seperators []byte = []byte{10, 32, 44, 46}
+var stopWords map[string]bool
+var dictionary map[string]*list.List
 
 func main() {
 	var corpusPath string
@@ -27,23 +30,50 @@ func dispatcher(corpusPath string) {
 	if err != nil {
 		panic(err)
 	}
+	collectStopWords()
+	dictionary = make(map[string]*list.List)
 	scanner := bufio.NewScanner(io.Reader(file))
 
 	scanner.Split(splitTokens)
 
-	var token string
-	var wg sync.WaitGroup
+	var (
+		token string
+		pos   uint32 = 0
+		wg    sync.WaitGroup
+	)
 	for scanner.Scan() {
 		token = scanner.Text()
 		if len(token) > 0 {
 			wg.Add(1)
-			go addToken(token, &wg)
+			go addToken(token, pos, &wg)
+			pos += 1
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 	wg.Wait()
+}
+
+func addToken(token string, pos uint32, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Check to see if token is a stop word
+	_, ok := stopWords[token]
+	if !ok {
+		// Stem the token
+		token = porterstemmer.StemString(token)
+		postingsList, ok := dictionary[token]
+		if ok {
+			postingsList.PushBack(pos)
+		} else {
+			l := list.New()
+			l.PushBack(pos)
+			dictionary[token] = l
+		}
+		e := dictionary[token].Front()
+		fmt.Println(e.Value)
+	}
 }
 
 func splitTokens(data []byte, atEOF bool) (advance int, token []byte, err error) {
@@ -79,8 +109,23 @@ func splitTokens(data []byte, atEOF bool) (advance int, token []byte, err error)
 	return
 }
 
-func addToken(token string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	token = porterstemmer.StemString(token)
-	fmt.Println(token)
+func collectStopWords() {
+	file, err := os.Open("stopwords")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	stopWords = make(map[string]bool)
+	scanner := bufio.NewScanner(io.Reader(file))
+
+	var word string
+	for scanner.Scan() {
+		word = scanner.Text()
+		if len(word) > 0 {
+			stopWords[word] = true
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
